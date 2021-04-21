597ae04915e7a66852b95172b3ffb97e4641b8eb
satoukatumi18 <satoukatumi18@gmail.com>
2021-04-21 23:47:45 +0900

    最初のコミット

---
+++ src/main/java/kun/randomtp1_16_5/Randomtp1_16_5.java
@@ -0,0 +1,17 @@
+package kun.randomtp1_16_5;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Randomtp1_16_5 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
