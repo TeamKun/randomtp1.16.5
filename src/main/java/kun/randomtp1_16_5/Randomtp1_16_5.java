ae6c2e91610b57a29c1ddd8bb2f1e1f58be224e6
sakastudio <sakastudio100@gmail.com>
2021-04-27 23:28:50 +0900

    コマンドあたり修正

--- src/main/java/kun/randomtp1_16_5/Randomtp1_16_5.java
+++ src/main/java/kun/randomtp1_16_5/Randomtp1_16_5.java
@@ -4,11 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
 public final class Randomtp1_16_5  extends JavaPlugin implements Listener {
@@ -30,33 +32,38 @@ public final class Randomtp1_16_5  extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-        //TODO Bukkit.selectEntities()を作ってセレクタ引数を追加する
         if(cmd.getName().equalsIgnoreCase("exerandomTp")){
+            if (args.length != 1){sender.sendMessage("/exerandomTp <プレイヤー>");return false;}
+
+            List<Entity> entities = Bukkit.selectEntities(sender,args[0]);
 
             Player p = (Player)sender;
             Random r = new Random();
             double playerX = p.getLocation().getX();
             double playerZ = p.getLocation().getZ();
 
-            Collection<? extends Player> players = Bukkit.getOnlinePlayers();
-
-            for (Player player : players) {
+            for (Entity entity : entities) {
                 double x = r.nextInt(radius * 2) - radius + playerX;
                 double z = r.nextInt(radius * 2) - radius + playerZ;
 
-                Location location = new Location(p.getWorld(), x,255,z);
-                player.teleport(location);
+                entity.teleport(new Location(p.getWorld(), x,255,z));
             }
-            sender.sendMessage("ランダムTPを実行しました");
+            sender.sendMessage(String.format("%d体のエンティティをランダムTPしました",entities.size()));
 
             return true;
         }
         if(cmd.getName().equalsIgnoreCase("setRadius")){
             if (args.length != 1){sender.sendMessage("/setRadius <TPの半径>");return false;}
 
-            radius = Integer.parseInt(args[0]);
-            sender.sendMessage("半径を" + args[0] + "に設定しました");
-            return true;
+            try {
+                radius = Integer.parseInt(args[0]);
+                sender.sendMessage("半径を" + args[0] + "に設定しました");
+                return true;
+
+            }catch (Exception e){
+                sender.sendMessage("/setRadius <TPの半径>");
+                return false;
+            }
         }
         return false;
     }
